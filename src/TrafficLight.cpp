1106287f749c98d47e86cec8938238cdc33f21d5
Ryan Potter <ryan.potter94@gmail.com>
2024-02-10 07:13:45 +0000

    Completed FP.4.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -11,16 +11,19 @@ T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
 }
+*/
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex)
+    _queue.push_back(std::move(msg));
+    _cond.notify_one
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
